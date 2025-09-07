1748a29aa6071401b3566996e533a987279e74ab
karishb <ikrishmabhatia@gmail.com>
2025-09-07 23:14:24 +0530

    config file

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,1 @@
-// Use local server for development, production server for deployment
-export const API_URL = __DEV__ 
-  ? 'http://localhost:3000/api' 
-  : 'https://macromuscles.onrender.com/api';
+export const API_URL = 'https://macromuscles.onrender.com/api';
